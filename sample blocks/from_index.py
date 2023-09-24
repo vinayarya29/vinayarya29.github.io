f1db5a8db977821acd9f05a16087b3dce68b833f
vinayarya29 <vinayarya29@gmail.com>
2023-09-24 22:01:16 +0530

    party_22sep

---
+++ sample blocks/from_index.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 24 21:51:42 2023
+
+@author: vinayarya
+"""
+
+<!-- Post -->
+
+
+ay, many thanks to <strong>regularjane</strong>
+hotos</a></h2>
+
+featured"><img src="images/pic05.jpg" alt="" /></a>
+heck out her work</h3>
+a id justo mattis pharetra. Fusce suscipit
+it amet mollis tortor congue. Sed quis mauris
+ristique. Curabitur leo nibh, rutrum eu malesuada
+em ipsum dolor sit amet lorem ipsum sed consequat
+rem ipsum consequat Phasellus laoreet massa id
+em ipsum dolor sit amet lorem ipsum sed consequat
+em sed tempus aliquam lorem ipsum veroeros
+rem ipsum consequat Phasellus laoreet massa id
+sce suscipit ligula vel quam viverra sit amet
+ quis mauris sit amet magna accumsan tristique.
+m malesuada.</p>
+ros consequat magna tempus lorem ipsum consequat
+d justo mattis pharetra. Fusce suscipit ligula
+ mollis tortor congue. Sed quis mauris sit amet
+ Curabitur leo nibh, rutrum eu malesuada in,
+tur leo nibh, rutrum eu malesuada in, tristique
+ sit amet lorem ipsum sed consequat magna
+ tempus aliquam lorem ipsum veroeros consequat
+
+
+utton icon solid fa-file">Continue Reading</a></li>
+
+
+<!-- Highlights -->
+
+
+
+
+
+ghlight">
+
+thing of note</a></h3>
+
+age left"><img src="images/pic06.jpg" alt="" /></a>
+eet massa id justo mattis pharetra. Fusce suscipit ligula vel quam
+is tortor congue magna lorem ipsum dolor et quisque ut odio facilisis
+nunc vel est suscipit convallis non id orci. Ut interdum tempus
+Etiam non nunc vel est suscipit convallis non id orci.</p>
+
+s="button icon solid fa-file">Learn More</a></li>
+
+
+
+
+
+
+
+ghlight">
+
+thing of less note</a></h3>
+
+age left"><img src="images/pic07.jpg" alt="" /></a>
+eet massa id justo mattis pharetra. Fusce suscipit ligula vel quam
+is tortor congue magna lorem ipsum dolor et quisque ut odio facilisis
+nunc vel est suscipit convallis non id orci. Ut interdum tempus
+Etiam non nunc vel est suscipit convallis non id orci.</p>
+
+s="button icon solid fa-file">Learn More</a></li>
+
+
+
+
+
+
